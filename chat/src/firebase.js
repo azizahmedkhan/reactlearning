77abdac705ac8f69be50f9cbf7e6fe8b4290c93f
Aziz Ahmed Khan <azizahmedkhan@users.noreply.github.com>
2019-12-14 16:53:31 +1300

    Channel substraction

---
+++ chat/src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyCD632_j6XaUVz5O1MqDOFbedl8GxCfzd4",
+  authDomain: "friendlychat-c9506.firebaseapp.com",
+  databaseURL: "https://friendlychat-c9506.firebaseio.com",
+  projectId: "friendlychat-c9506",
+  storageBucket: "friendlychat-c9506.appspot.com",
+  messagingSenderId: "570826381967",
+  appId: "1:570826381967:web:71389aad5fce02fc012efa"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+export { db };
