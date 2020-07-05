b1c1c9e646cba5fefdaeaa2ed8b803c20f80b785
Aziz <admin@Testimac.hub>
2020-07-06 10:43:41 +1200

    after status update

--- chat/functions/index.js
+++ chat/functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+//initilize app doesn't need config here in the server.
+admin.initializeApp()
+
+const db= admin.firestore()
+
+exports.onUserStatusChanges = functions.database
+                                .ref(`status/{userId}`)
+                                .onUpdate((change, context) => {
+                                    const eventStatus = change.after.val()
+                                    const userDoc = db.doc(`users/{context.params.userId}`)
+                                    //user can be online to Offline aswell
+                                    //change is change to real time database
+                                    return change.after.ref.once("value").then(snapshot => {
+                                        const status= snapshot.val()
+                                        if(status.lastChanged > eventStatus.lastChanged) {
+                                            // if our current value is more recent than don't do any thing.
+                                            return
+                                        }
+                                    })
+                                    eventStatus.lastChanged =  new Date(eventStatus.lastChanged)
+                                    userDoc.update({status:eventStatus})
+                                })
 
 // // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// // https://firebase.google.com/docs/functions/write-firebase-functions          .
